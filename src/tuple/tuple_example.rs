53ffbbe38f008b50d1a438799ffb5a11d1676fe4
Francisco Arenas <franciscoarenas.globant@latam.com>
2024-06-12 23:06:43 -0400

    add one example for tuple x, y, z

--- src/tuple/tuple_example.rs
+++ src/tuple/tuple_example.rs
@@ -1,6 +1,10 @@
 pub fn tuple_example() {
     let tup = (23, "variable Y");
-
     let (x, y) = tup;
     println!("x: {} and y: {}", x, y);
+
+    let tup = (44, 33, 22);
+    let (x, y, z) = tup;
+    println!("x: {} and y: {} and z: {}", x, y, z);
+
 }
